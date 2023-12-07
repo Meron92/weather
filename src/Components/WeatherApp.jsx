45e91abb46aaee7d23c6bb528c52253b1e458178
Meron92 <Meron_Mekonnen@hotmail.com>
2023-12-07 13:49:15 +0100

    test

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -21,7 +21,7 @@ const WeatherApp = () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(response);
+
     const humidity = document.getElementsByClassName("humidity-percent");
     const wind = document.getElementsByClassName("wind-speed");
     const temp = document.getElementsByClassName("weather-temp");
